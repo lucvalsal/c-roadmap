3d8c5d454af1b3a6486f367ad6939b2218a9e854
Lucas Valdez Salinas <lucasvaldezsalinas@gmail.com>
2026-08-10 16:37:34 -0300

    Day 6 - Practice Day - Refactor with function to count up and down

--- w1/d4/count_reverse.c
+++ w1/d4/count_reverse.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+void count_up(int start_num, int end_num) {
+	if (start_num < end_num) {
+		for (start_num; start_num < end_num; start_num++) {
+			printf("Number %d\n", start_num);
+		}
+	} else {
+		printf("The start number must be lower than the end number...\n");
+	}
+
+	return;
+}
+
+void count_down(int start_num, int end_num) {
+	if (start_num > end_num) {
+		for (start_num; start_num > end_num; start_num--) {
+			printf("Number %d\n", start_num);
+		}
+	} else {
+		printf("The start number must be greater than the end number...\n");
+	}
+}
+
 int main() {
 	int a = 10;
 	// Counting loop
@@ -15,4 +37,19 @@ int main() {
 		printf("Reverse @ %d\n", a);
 		a--;
 	}
+
+	printf("\n");
+
+	printf("Now with functions...\n");
+
+
+	printf("\n");
+
+	printf("--Count Up--\n");
+	count_up(5, 15);
+
+	printf("\n");
+
+	printf("--Count Down--\n");
+	count_down(15, 5);
 }
